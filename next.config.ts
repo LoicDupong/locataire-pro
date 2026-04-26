b7c6d372c97c485a1f4e5b56e6b0531d27491cb4
LoicDupong <loic@dupong.be>
2026-04-26 22:58:57 +0200

    feat: configure Next.js image settings for SVG support and security policies

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,9 @@
 import type { NextConfig } from 'next'
-const nextConfig: NextConfig = {}
+const nextConfig: NextConfig = {
+  images: {
+    dangerouslyAllowSVG: true,
+    contentDispositionType: 'attachment',
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
+}
 export default nextConfig
